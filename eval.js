3074614ac5976d2cd41c2540560055cf766f19b9
Yu Hsu <yhsu@kbbh.org>
2021-03-29 16:18:23 -0700

    Attempting to speed up form by calculating scores on scores fields comming into and out of view. Version 3.

--- eval.js
+++ eval.js
@@ -333,34 +333,7 @@ function customCallBack ()
 	});
 
 	//Calcuate evaluation scores when score fields come into focus
-	$('tr').has('div[type=answer]').find('input').focus(function()
-	{
-		empTotal = 0;
-		for(loopCount = 0; loopCount < $('tr').has('div[type*=emp]').find('input[name*=_calc]').length; loopCount++)
-		{
-			empTotal = parseInt(empTotal) + parseInt($('tr').has('div[type*=emp]').find('input[name*=_calc]').eq(loopCount).val());
-		}
-		empScore = (parseInt(empTotal) / parseInt($('tr').has('div[type*=emp]').find('input[name*=_calc]').length)).toFixed(1);
-		$('tr').has('div[id=in1]').find('input').val(empScore + ' / 5');
-		proTotal = 0;
-		for(loopCount = 0; loopCount < $('tr').has('div[type*=pro]').find('input[name*=_calc]').length; loopCount++)
-		{
-			proTotal = parseInt(proTotal) + parseInt($('tr').has('div[type*=pro]').find('input[name*=_calc]').eq(loopCount).val());
-		}
-		proTotal = (parseInt(proTotal) / parseInt($('tr').has('div[type*=pro]').find('input[name*=_calc]').length)).toFixed(1);
-		$('tr').has('div[id=in2]').find('input').val(proTotal + ' / 5');
-
-		supTotal = 0;
-		for(loopCount = 0; loopCount < $('tr').has('div[type=sup]').find('input[name*=_calc]').length; loopCount++)
-		{
-			supTotal = parseInt(supTotal) + parseInt($('tr').has('div[type*=sup]').find('input[name*=_calc]').eq(loopCount).val());
-		}
-		supTotal = (parseInt(supTotal) / parseInt($('tr').has('div[type=sup]').find('input[name*=_calc]').length)).toFixed(1);
-		$('tr').has('div[id=in3]').find('input').val(supTotal + ' / 5');
-	});
-
-	//Calcuate evaluation scores when score fields come out of focus
-	$('tr').has('div[type=answer]').find('input').blur(function()
+	$('tr').has('div[type=answer]').find('input').scroll(function()
 	{
 		empTotal = 0;
 		for(loopCount = 0; loopCount < $('tr').has('div[type*=emp]').find('input[name*=_calc]').length; loopCount++)
