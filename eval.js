ad8193788d721fe0bca5e1ca3f0bd58a0f6a1e09
Yu Hsu <yhsu@kbbh.org>
2021-03-29 16:26:54 -0700

    Going back to calculating scores when an answer is clicked.

--- eval.js
+++ eval.js
@@ -330,9 +330,43 @@ function customCallBack ()
 		}
 		$('tr').has('div[id=autoGenerate]').next().find('textarea').trigger('change');
 		setTimeout(resize($('tr').has('div[id=autoGenerate]').next().find('textarea')[0]), 0);
+
+		//Calcuate evaluation scores each time questions are clicked
+		if($('tr').has('input[id=' + this.name + ']').find('div[class=Q]').attr('type').includes('emp'))
+		{
+			empTotal = 0;
+			for(loopCount = 0; loopCount < $('tr').has('div[type*=emp]').find('input[name*=_calc]').length; loopCount++)
+			{
+				empTotal = parseInt(empTotal) + parseInt($('tr').has('div[type*=emp]').find('input[name*=_calc]').eq(loopCount).val());
+			}
+			}
+			empScore = (parseInt(empTotal) / parseInt($('tr').has('div[type*=emp]').find('input[name*=_calc]').length)).toFixed(1);
+			$('tr').has('div[id=in1]').find('input').val(empScore + ' / 5');
+		}
+		if($('tr').has('input[id=' + this.name + ']').find('div[class=Q]').attr('type').includes('pro'))
+		{
+			proTotal = 0;
+			for(loopCount = 0; loopCount < $('tr').has('div[type*=pro]').find('input[name*=_calc]').length; loopCount++)
+			{
+				proTotal = parseInt(proTotal) + parseInt($('tr').has('div[type*=pro]').find('input[name*=_calc]').eq(loopCount).val());
+			}
+			proTotal = (parseInt(proTotal) / parseInt($('tr').has('div[type*=pro]').find('input[name*=_calc]').length)).toFixed(1);
+			$('tr').has('div[id=in2]').find('input').val(proTotal + ' / 5');
+		}
+		if($('tr').has('input[id=' + this.name + ']').find('div[class=Q]').attr('type') == 'sup')
+		{
+			supTotal = 0;
+			
+			for(loopCount = 0; loopCount < $('tr').has('div[type=sup]').find('input[name*=_calc]').length; loopCount++)
+			{
+				supTotal = parseInt(supTotal) + parseInt($('tr').has('div[type*=sup]').find('input[name*=_calc]').eq(loopCount).val());
+			}
+			supTotal = (parseInt(supTotal) / parseInt($('tr').has('div[type=sup]').find('input[name*=_calc]').length)).toFixed(1);
+			$('tr').has('div[id=in3]').find('input').val(supTotal + ' / 5');
+		}
 	});
 
-	//Calcuate evaluation scores when score fields come into focus
+	/*//Calcuate evaluation scores when score fields come into focus
 	$('tr').has('div[type=answer]').find('input').focusin(function()
 	{
 		empTotal = 0;
@@ -386,7 +420,7 @@ function customCallBack ()
 		}
 		supTotal = (parseInt(supTotal) / parseInt($('tr').has('div[type=sup]').find('input[name*=_calc]').length)).toFixed(1);
 		$('tr').has('div[id=in3]').find('input').val(supTotal + ' / 5');
-	});
+	});*/
 
 	//Load values from qArray into Improvement Plan on page submit
 	$('input[name=Complete]').click(function()
