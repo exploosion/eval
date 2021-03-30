f2e8992a879564e39a2f99274ff899263445a4bc
Yu Hsu <yhsu@kbbh.org>
2021-03-30 09:27:02 -0700

    On page load check to see if question answers have values, and load into oObject. Version 3.

--- eval.js
+++ eval.js
@@ -112,6 +112,15 @@ function customCallBack ()
 		$('div[id=plan]').html($('div[id=plan]').html() + ( '<div class=\'areas\' id=\'area' + loopCount + '\'>' + '<label id-=\'' + 'label' + loopCount + '\'><b>Improvement Area: </b></label>' + qObject[loopCount].q + '<br>') + ('<label><b>Target Date:</b></label><br><input class=\'dates\'id=\'' + 'date' + loopCount + '\' type=\'date\'></input><br>') + ('<label><b>Plan:</b></label><br><textarea class=\'plans\' id=\'' + 'plan' + loopCount + '\' cols=\'71\'></textarea><br><br>' + '</div>'));
 	}
 
+	//Prevent use of new line in temp Improvement Plan text areas
+	$('.plans').keydown(function(e)
+	{
+		if (e.keyCode == 13 && !e.shiftKey)
+		{
+			e.preventDefault();
+		}
+	});
+	
 	//Events to load temp values into qObject
 	$('.dates').change(function()
 	{
@@ -424,15 +433,6 @@ function customCallBack ()
 	}
 	$('tr').has('div[id=additionalAreas]').next().find('textarea').trigger('change');
 	setTimeout(resize($('tr').has('div[id=additionalAreas]').next().find('textarea')[0]), 0);
-
-	//Prevent use of new line in temp Improvement Plan text areas
-	$('.plans').keydown(function(e)
-	{
-		if (e.keyCode == 13 && !e.shiftKey)
-		{
-			e.preventDefault();
-		}
-	});
 
 	//Event handle to prevent non numerics and only allow one decimal in clinical scores and Custome Metric values
 	$('tr').has('div[class=calc]').find('input').keyup(preventAlpha);
